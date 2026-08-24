ff5e09986ed14ea82ea298341b68179087d5bea7
Hiba Ibrahim Khanfar <hibaibraheem140@gmail.com>
2026-08-24 13:03:57 +0300

    basic login

--- apps/authentication/views.py
+++ apps/authentication/views.py
@@ -2,6 +2,10 @@ from rest_framework import status, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import RegisterSerializer
+from apps.accounts.models import User
+from rest_framework.views import APIView
+from rest_framework.exceptions import AuthenticationFailed
+
 
 class RegisterView(APIView):
   permission_classes = [permissions.AllowAny]
@@ -18,3 +22,24 @@ class RegisterView(APIView):
               status=status.HTTP_201_CREATED
           )
       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user=User.objects.filter(email=email).first()
+        if user is None:
+            raise AuthenticationFailed('User not found')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password')
+
+        return Response(
+            {
+                "message": "WELCOME",
+                "id": user.id,
+                "email": user.email,
+            },
+            status=status.HTTP_200_OK
+        )
